532f12ff86560bbb798f7f167ea56358a39f39ae
miro <jarbasai@mailfence.com>
2024-07-19 23:59:34 +0100

    :tada:

---
+++ ovos_voice_embeddings/__init__.py
@@ -0,0 +1,42 @@
+import numpy as np
+from ovos_chromadb_embeddings import ChromaEmbeddingsDB
+from ovos_plugin_manager.templates.embeddings import VoiceEmbeddingsRecognizer
+from resemblyzer import VoiceEncoder, preprocess_wav
+from speech_recognition import Recognizer, AudioFile, AudioData
+
+
+class VoiceEmbeddingsRecognitionPlugin(VoiceEmbeddingsRecognizer):
+    def __init__(self, thresh: float = 0.75):
+        path = "/tmp/voice_db"  # TODO
+        db = ChromaEmbeddingsDB(path)
+        super().__init__(db, thresh)
+        self.encoder = VoiceEncoder()
+
+    def get_voice_embeddings(self, audio_data: np.ndarray) -> np.ndarray:
+        """audio data from a OVOS microphone"""
+        if isinstance(audio_data, AudioData):
+            audio_data = audio.get_wav_data()
+        if isinstance(audio_data, bytes):
+            audio_data = self.audiochunk2array(audio_data)
+        return self.encoder.embed_utterance(audio_data)
+
+
+if __name__ == "__main__":
+    # Example usage:
+
+    v = VoiceEmbeddingsRecognitionPlugin()
+
+    a = "/home/miro/PycharmProjects/ovos-user-id/2609-156975-0001.flac"
+    b = "/home/miro/PycharmProjects/ovos-user-id/qCCWXoCURKY.mp3"
+    b2 = "/home/miro/PycharmProjects/ovos-user-id/4glfwiMXgwQ.mp3"
+
+    with AudioFile(a) as source:
+        audio = Recognizer().record(source)
+    v.add_voice("user", audio)
+
+    wav = preprocess_wav(b)
+    v.add_voice("donald", wav)
+
+    wav = preprocess_wav(b2)
+    print(v.predict(wav))
+    print(v.query(wav))
