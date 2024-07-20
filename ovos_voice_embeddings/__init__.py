988fae8341044347bf97270a31c990ab131e2631
miro <jarbasai@mailfence.com>
2024-07-20 17:07:24 +0100

    accept str path to chromadb

--- ovos_voice_embeddings/__init__.py
+++ ovos_voice_embeddings/__init__.py
@@ -1,5 +1,5 @@
 import os
-from typing import Optional
+from typing import Optional, Union
 
 import numpy as np
 from ovos_chromadb_embeddings import ChromaEmbeddingsDB
@@ -11,15 +11,15 @@ from speech_recognition import Recognizer, AudioFile, AudioData
 
 
 class VoiceEmbeddingsRecognitionPlugin(VoiceEmbeddingsStore):
-    def __init__(self, db: Optional[EmbeddingsDB] = None, thresh: float = 0.75):
+    def __init__(self, db: Optional[Union[EmbeddingsDB, str]] = None):
         if db is None:
             db_path = get_xdg_cache_save_path("chromadb")
             os.makedirs(db_path, exist_ok=True)
-            db_path = f"{db_path}/resemblyzer_voice_prints"
-            LOG.info(f"Using chromadb as voice embeddings store: {db_path}")
-            db = ChromaEmbeddingsDB(db_path)
-
-        super().__init__(db, thresh)
+            db = f"{db_path}/resemblyzer_voice_prints"
+        if isinstance(db, str):
+            LOG.info(f"Using chromadb as voice embeddings store: {db}")
+            db = ChromaEmbeddingsDB(db)
+        super().__init__(db)
         self.encoder = VoiceEncoder()
 
     def get_voice_embeddings(self, audio_data: np.ndarray) -> np.ndarray:
